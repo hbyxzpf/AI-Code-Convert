513a21e30bdf163c0962c3da36767ec0c634fea6
zhaopengfei <hbyx.zpf@gmail.com>
2023-07-26 15:37:05 +0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,19 +115,39 @@ export default function Home() {
 		<link rel="canonical" href="https://aicodeconvert.com" />
         <link rel="icon" href="/code.png" />
       </Head>
-	  
+
 	  <div className="h-100 flex justify-start items-center pl-10 pt-5 bg-[#0E1117]">
 	  	<img className="w-50 h-50" alt="AICodeConverter" src="code.png" />
 	  	<h1 className="text-white font-bold text-2xl"><span className="text-blue-500 ml-2">AI</span>CodeConvert.com</h1>
 	  </div>
-	  
+
       <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
 		<div className="mt-5 flex flex-col items-center justify-center sm:mt-10">
           <h1 className="text-4xl font-bold"><span className="text-blue-500">AI</span> Code Converter</h1>
 		  <div className="mt-5 text-xl text-center leading-2">Generate <span className="text-blue-500 font-bold">Code</span> or <span className="text-blue-500 font-bold">Natural Language</span> To
 		  <br /> Programming Language <span className="text-blue-500 font-bold">Code</span> of Selected</div>
         </div>
-		
+        <div className="mt-5 text-center text-sm">
+          {loading
+              ? '...'// Generating
+              : hasTranslated
+                  ? 'Output copied to clipboard!'
+                  : 'Enter some code and click "Generate"'}
+        </div>
+
+        <div className="mt-5 flex items-center space-x-2">
+          <button
+              className="w-[140px] cursor-pointer rounded-md bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600 active:bg-blue-700"
+              onClick={() => handleTranslate()}
+              disabled={loading}
+          >
+            {loading ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        {/*<button className="mt-2 cursor-pointer rounded-md bg-blue-800 px-4 py-2 hover:bg-blue-600 active:bg-blue-700">*/}
+        {/*	<a href="https://base64.kr/en">Visit Base64.kr</a>*/}
+        {/*</button>*/}
+      </div>
         <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
           <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
             <div className="text-center text-xl font-bold">From</div>
@@ -180,28 +200,6 @@ export default function Home() {
             )}
           </div>
         </div>
-		
-		<div className="mt-5 text-center text-sm">
-		  {loading
-		    ? '...'// Generating
-		    : hasTranslated
-		    ? 'Output copied to clipboard!'
-		    : 'Enter some code and click "Generate"'}
-		</div>
-		
-		<div className="mt-5 flex items-center space-x-2">
-		  <button
-		    className="w-[140px] cursor-pointer rounded-md bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600 active:bg-blue-700"
-		    onClick={() => handleTranslate()}
-		    disabled={loading}
-		  >
-		    {loading ? 'Generating...' : 'Generate'}
-		  </button>
-		</div>
-		<button className="mt-2 cursor-pointer rounded-md bg-blue-800 px-4 py-2 hover:bg-blue-600 active:bg-blue-700">
-			<a href="https://base64.kr/en">Visit Base64.kr</a>
-		</button>
-	  </div>
     </>
   );
 }
